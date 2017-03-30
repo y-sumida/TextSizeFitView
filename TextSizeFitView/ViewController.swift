47198438000b2c6f5b766196db84e992e8969794
Yuki Sumida <y.sumida.0201@gmail.com>
2017-03-30 13:05:46 +0900

    コードから利用する

--- TextSizeFitView/ViewController.swift
+++ TextSizeFitView/ViewController.swift
@@ -9,11 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var textSizeFitView: TextSizeFitView!
+    @IBOutlet weak var parentView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        textSizeFitView.setTitle(title: "ほげほげ")
+
+        let view1 = TextSizeFitView()
+        view1.setTitle(title: "ビュー１")
+        view1.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(view1)
+        
+        view1.leadingAnchor.constraint(equalTo: self.view.leadingAnchor,constant: 30.0).isActive = true
+        view1.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100.0).isActive = true
+        view1.widthAnchor.constraint(equalToConstant: view1.frame.width).isActive = true
+        view1.heightAnchor.constraint(equalToConstant: view1.frame.height).isActive = true
+
+        
+        let view2 = TextSizeFitView()
+        view2.setTitle(title: "コードから")
+        view2.translatesAutoresizingMaskIntoConstraints = false
+        self.parentView.addSubview(view2)
+    
+        view2.leadingAnchor.constraint(equalTo: self.parentView.leadingAnchor,constant: 30.0).isActive = true
+        view2.topAnchor.constraint(equalTo: self.parentView.topAnchor, constant: 10.0).isActive = true
+        view2.widthAnchor.constraint(equalToConstant: view2.frame.width).isActive = true
+        view2.heightAnchor.constraint(equalToConstant: view2.frame.height).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
